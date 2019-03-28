bf4a44b2ff4dd68201b38f1aae6d00babf34e2ed
Kanishk Kakar <kanishk.kakar@gmail.com>
2019-03-28 14:52:29 +0530

    Setup .js script for creating collections
    Hide sensitive info from git

---
+++ init_mongo.js
@@ -0,0 +1,36 @@
+const MongoClient = require("mongodb").MongoClient;
+const MongoServer = require("mongodb").Server;
+const assert = require("assert");
+const Constants = require("./constants");
+
+const dbType = process.argv[2];
+
+const mongoClient = new MongoClient(new MongoServer("localhost", 27017), {
+  useNewUrlParser: true
+});
+
+mongoClient.connect((err, mongoClient) => {
+  assert.equal(null, err);
+  // create all collections on the server
+  let database = null;
+  if (dbType === "p") {
+    database = mongoClient.db(Constants.productionDatabase);
+  } else {
+    database = mongoClient.db(Constants.stagingDatabase);
+  }
+  const collections = database.listCollections().toArray((err, columnInfo) => {
+    assert.equals(null, err);
+  });
+  const newCollections = Constants.collections;
+  for (collection in newCollections) {
+    if (collections.indexOf(collection) === -1) {
+      // collection not present, create new
+      database.createCollection(collection, (err, res) => {
+        if (err) {
+          console.err("Failed to create collection " + collection);
+          console.err(err);
+        }
+      });
+    }
+  }
+});
